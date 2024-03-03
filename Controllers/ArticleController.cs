a729b8e88750c7c29895fbe1156144af900b36c1
khanhbui <khanhbui.dubai@gmail.com>
2024-03-03 13:56:06 +0700

    update- repo- controller-article

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -1,12 +1,204 @@
-﻿namespace EnterpriceWeb.Controllers
+﻿using EnterpriceWeb.Models;
+using EnterpriceWeb.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnterpriceWeb.Controllers
 {
-    public class ArticleController
+    public class ArticleController : Controller
     {
-        //CreateArticle index
+        private AppDbConText _dbContext;
+        private RepoArticle _repoArticle;
+        public ArticleController(AppDbConText dbContext)
+        {
+            _dbContext = dbContext;
+            _repoArticle = new RepoArticle(dbContext);
+        }
 
-        //UpdateArticle index
+        [HttpGet]
+        public async Task<IActionResult> IndexArticle()
+        {
+            List<Article> list_Article = await _repoArticle.SearhAllArticle();
+            return View(list_Article);
+        }
 
-        //DeleteArticle index
+
+        [HttpGet]
+        public IActionResult CreateArticle()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> CreateArticle(string ip_name)
+        //{
+        //    Faculty searchName = await _repoFaculty.SearhFacultyByName(ip_name);
+        //    if (searchName == null)
+        //    {
+        //        HandleCreateArticle(ip_name);
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("CreateFaculty");
+        //    }
+        //    return RedirectToAction("IndexFaculty");
+        //}
+
+        //private void HandleCreateArticle(string ip_name)
+        //{
+        //    Faculty faculty = new Faculty();
+        //    faculty.f_name = ip_name;
+        //    _dbContext.Add(faculty);
+        //    _dbContext.SaveChanges();
+        //}
+
+        //[HttpGet]
+        //public IActionResult UpdateFaculty()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public async Task<IActionResult> UpdateFaculty(string ip_name, int id)
+        //{
+        //    Faculty oldFaculty = await _repoFaculty.SearhFacultyById(id);
+        //    if (oldFaculty != null)
+        //    {
+        //        HandleUpdateFactulty(oldFaculty, ip_name);
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("UpdateFaculty");
+        //    }
+
+        //    return RedirectToAction("IndexFaculty");
+        //}
+
+        //private void HandleUpdateFactulty(Faculty faculty, string ip_name)
+        //{
+        //    faculty.f_name = ip_name;
+        //    _dbContext.Update(faculty);
+        //    _dbContext.SaveChanges();
+        //}
+
+        //[HttpPost]
+        //public async Task<IActionResult> DeleteFaculty(int facultyId)
+        //{
+        //    Faculty faculty = await _repoFaculty.SearhFacultyById(facultyId);
+        //    if (faculty != null)
+        //    {
+        //        HandleDeleteFactulty(faculty);
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("IndexFaculty");
+        //    }
+        //    return RedirectToAction("IndexFaculty");
+        //}
+
+        //private void HandleDeleteFactulty(Faculty faculty)
+        //{
+        //    faculty.f_status = "0";
+        //    _dbContext.Update(faculty);
+        //    _dbContext.SaveChanges();
+        //}
+        //private AppDbConText _dbContext;
+        //private RepoFaculty _repoFaculty;
+        //public FacultyController(AppDbConText dbContext)
+        //{
+        //    _dbContext = dbContext;
+        //    _repoFaculty = new RepoFaculty(dbContext);
+        //}
+
+
+
+        //[HttpGet]
+        //public async Task<IActionResult> IndexFaculty()
+        //{
+        //    List<Faculty> list_faculty = await _repoFaculty.SearhAllFaculty();
+        //    return View(list_faculty);
+        //}
+
+
+        //[HttpGet]
+        //public IActionResult CreateFaculty()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public async Task<IActionResult> CreateFaculty(string ip_name)
+        //{
+        //    Faculty searchName = await _repoFaculty.SearhFacultyByName(ip_name);
+        //    if (searchName == null)
+        //    {
+        //        HandleCreateFactulty(ip_name);
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("CreateFaculty");
+        //    }
+        //    return RedirectToAction("IndexFaculty");
+        //}
+
+        //private void HandleCreateFactulty(string ip_name)
+        //{
+        //    Faculty faculty = new Faculty();
+        //    faculty.f_name = ip_name;
+        //    _dbContext.Add(faculty);
+        //    _dbContext.SaveChanges();
+        //}
+
+        //[HttpGet]
+        //public IActionResult UpdateFaculty()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public async Task<IActionResult> UpdateFaculty(string ip_name, int id)
+        //{
+        //    Faculty oldFaculty = await _repoFaculty.SearhFacultyById(id);
+        //    if (oldFaculty != null)
+        //    {
+        //        HandleUpdateFactulty(oldFaculty, ip_name);
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("UpdateFaculty");
+        //    }
+
+        //    return RedirectToAction("IndexFaculty");
+        //}
+
+        //private void HandleUpdateFactulty(Faculty faculty, string ip_name)
+        //{
+        //    faculty.f_name = ip_name;
+        //    _dbContext.Update(faculty);
+        //    _dbContext.SaveChanges();
+        //}
+
+        //[HttpPost]
+        //public async Task<IActionResult> DeleteFaculty(int facultyId)
+        //{
+        //    Faculty faculty = await _repoFaculty.SearhFacultyById(facultyId);
+        //    if (faculty != null)
+        //    {
+        //        HandleDeleteFactulty(faculty);
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("IndexFaculty");
+        //    }
+        //    return RedirectToAction("IndexFaculty");
+        //}
+
+        //private void HandleDeleteFactulty(Faculty faculty)
+        //{
+        //    faculty.f_status = "0";
+        //    _dbContext.Update(faculty);
+        //    _dbContext.SaveChanges();
+        //}
+
 
 
         //CreateFeedback
--- Repository/RepoArticle.cs
+++ Repository/RepoArticle.cs
@@ -1,7 +1,29 @@
-﻿namespace EnterpriceWeb.Repository
+﻿using EnterpriceWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnterpriceWeb.Repository
 {
     public class RepoArticle
     {
-
+        private readonly AppDbConText _appDBContext;
+        public RepoArticle(AppDbConText appDBContext)
+        {
+            this._appDBContext = appDBContext;
+        }
+        public async Task<List<Article>> SearhAllArticleAccept()
+        {
+            List<Article> article = await _appDBContext.articles.Where(art => art.article_status.Equals("1")).ToListAsync();
+            return article;
+        }
+        public async Task<List<Article>> SearhAllArticle()
+        {
+            List<Article> article = await _appDBContext.articles.ToListAsync();
+            return article;
+        }
+        public async Task<Article> SearhArticleById(int id)
+        {
+            Article article = await _appDBContext.articles.Where(art => art.article_id.Equals(id) && art.article_status.Equals("1")).FirstOrDefaultAsync();
+            return article;
+        }
     }
 }
