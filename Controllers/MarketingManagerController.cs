0b7eea8bff670cdf7cd7635a2e1eb8484a1e14b2
phihung <tphphihung@gmail.com>
2024-03-06 19:49:21 +0700

    fix

---
+++ Controllers/MarketingManagerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnterpriceWeb.Controllers
+{
+    public class MarketingManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
