34fd83ecfeabd0394650d1fead689946275306dd
duy <106375074+duy3167@users.noreply.github.com>
2024-03-22 15:22:45 +0700

    update-repo

--- Repository/RepoAccount.cs
+++ Repository/RepoAccount.cs
@@ -19,7 +19,7 @@ namespace EnterpriceWeb.Repository
         }
         public async Task<List<User>> SearAllhUserById(int id)
         {
-            List<User> users = await appDBContext.users.Where(us => us.f_id.Equals(id)).Include(us => us.faculty).ToListAsync();
+            List<User> users = await appDBContext.users.Where(us => us.f_id.Equals(id) && us.us_role.Equals("student")).Include(us => us.faculty).ToListAsync();
             return users;
         }
         public async Task<string> SearchRoleUser(int userId)
